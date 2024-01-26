50522c7d56a612cab5253ba48da3c543dfad4fde
Nashid K <kp.nashid.nashid@gmail.com>
2024-01-26 23:05:37 +0530

    Chess board created

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const gameboard=document.querySelector(".board")
 const numbers=document.querySelector(".numbers")
-const letters.document.querySelector(".letters")
+const letters=document.querySelector(".letters")
 
+let white=true;
+
+for (let i=1; i<=64; i++)
+{
+    let square=document.createElement("div")
+    square.classList.add("square")
+    if(!white)
+    {
+        square.classList.add("black")
+    }
+    white=!white
+    if (i%8===0)
+    {
+        white=!white
+    }
+    gameboard.appendChild(square)
+}
